6bc4a7de1696eca04cae0214580cca45df63fc2b
lihe07 <li@imlihe.com>
2022-07-18 11:30:40 +0800

    Update README, passing tests

--- src/i18n.js
+++ src/i18n.js
@@ -2,8 +2,6 @@ import { createI18n } from 'vue-i18n'
 import messages from './messages.json'
 
 export default createI18n({
-  // ..
-  locale: localStorage.getItem('locale') ? localStorage.getItem('locale') : navigator.language.split('-')[0],
   fallbackLocale: 'en',
   legacy: false,
   globalInjection: true,
