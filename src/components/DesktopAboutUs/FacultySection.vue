38a8c6776a59dcf6c85385cf06525413b90f9a1c
Henry3510 <30743133+Henry3510@users.noreply.github.com>
2022-08-04 10:37:34 +0800

    Update AboutUs

---
+++ src/components/DesktopAboutUs/FacultySection.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="section">
+        <div id="faculty" class="p-y-10 font-sans flex-col">
+            <h2 class="title"> {{ $t("AboutUs.Faculty.Title") }}</h2>
+            <div class="flex items-center p-y-10 text-center">
+                <div style="width: 50%;" class="ratio">{{ $t("AboutUs.Faculty.Ratio") }}</div>
+                <div style="width: 50%;">{{ $t("AboutUs.Faculty.Content") }}</div>
+            </div>
+        </div>
+
+    </section>
+</template>
+
+
+<script>
+import homeBg1 from '../../assets/images/homeBg1.jpg?webp'
+</script>
+<style>
+.ratio {
+    font-size: 4rem;
+    font-weight: 700;
+}
+</style>
