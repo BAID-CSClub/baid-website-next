daba80aabedf7b9027f64632f4586b5ed9ba237e
ShepherdShepherd <91712540+ShepherdShepherd@users.noreply.github.com>
2022-08-08 10:33:02 +0800

    Add files via upload

---
+++ src/views/desktop/EducationTeaching.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <Curriculums />
+        <LearningMethod />
+        <StudentGuidence />
+        <WonderfulMoment />
+    </div>
+</template>
+<script setup>
+    import Curriculums from '../../components/DesktopEducationTeaching/Curriculums.vue'
+    import LearningMethod from '../../components/DesktopEducationTeaching/LearningMethod.vue'
+    import StudentGuidence from '../../components/DesktopEducationTeaching/StudentGuidence.vue'
+    import WonderfulMoment from '../../components/DesktopEducationTeaching/WonderfulMoment.vue'
+</script>
