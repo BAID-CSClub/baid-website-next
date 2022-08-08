055c03c4f3f844153cbb0eafb86aa3a34fa26f34
ShepherdShepherd <91712540+ShepherdShepherd@users.noreply.github.com>
2022-08-08 10:46:36 +0800

    Add files via upload

---
+++ src/components/DesktopEducationTeaching/LearningMethod.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="m-3vw" style="margin-top:-5vw">
+    <div class="text-10 font-700 text-right" style="margin-right:8vw;">
+        学习方式
+    </div>
+        <div class="grid h-44vw" style="margin:1vw 8vw;grid-auto-flow: column;grid-template-columns:2.5fr 1fr;gap:15px;">
+            <div class="bg-purple flex">
+                <div class="w-90% text-white" style="position:relative;top:30vw;left:2vw">
+                    <div class="text-9">自处探索多样的学习方式</div>
+                    <br />
+                    <div class="text-4.5">学习方式自主探索多样的学习方式是怎么回事呢？学习方式相信大家都很熟悉，但是学习方式自主探索多样的学习方式是怎么回事呢，下面就让小编带大家一起了解吧。</div>
+                </div>
+            </div>
+            <div class="grid" style="gap:15px">
+                <div class="bg-pink"></div>
+                <div class="bg-purple"></div>
+                <div class="bg-fuchsia"></div>
+            </div>
+        </div>
+    
+</div>
+</template>
+<script setup>
+</script>
+<style scoped>
+</style>
---
+++ src/components/DesktopEducationTeaching/WonderfulMoment.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="bg-pink h-46vw grid" style="grid-template-columns:1fr 0.5fr 0.5fr 1fr;grid-template-rows:1fr 0.6fr 1fr 0.6fr;margin-bottom:120px">
+        <div class="standard-red"></div> 
+        <div class="bg-purple"></div>
+        <div class="bg-pink"></div>
+        <div class="bg-blue"></div>
+        <div class="bg-black"></div>
+        <div class="bg-green"></div>
+        <div class="bg-white"></div>
+        <div class="bg-yellow grid" style="grid-auto-flow: column;">
+            <div class="bg-green"></div>
+            <div class="grid">
+                <div class="bg-yellow"></div>
+                <div class="bg-white"></div>
+            </div>
+        </div>
+        <div class="bg-yellow grid" style="grid-auto-flow: column;">
+            <div class="absolute bg-pink"></div>
+            <div class="bg-white"></div>
+            <div class="bg-yellow"></div>
+        </div>
+        <div class="bg-green"></div>
+        <div class="bg-white"></div>
+        <div class="bg-black"></div>
+        <div class="bg-yellow grid" style="grid-auto-flow: column;">
+            <div class="bg-white"></div>
+            <div class="bg-blue"></div>
+        </div>
+        <div class="bg-white"></div>
+        <div class="bg-green"></div>
+        <div class="bg-red"></div>
+    </div>
+</template>
+<script setup>
+</script>
