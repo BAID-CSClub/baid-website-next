bd57ef2b20db9cd8b37e02d36c95de1c03363c55
lihe <lihelihe2007@qq.com>
2022-08-09 15:44:34 +0800

    Add EducationTeaching

--- src/views/desktop/EducationTeaching.vue
+++ src/views/desktop/EducationTeaching.vue
@@ -1,3 +1,14 @@
 <template>
-  <div></div>
+    <div>
+        <CurriculumsIntroduction />
+        <LearningMethod />
+        <StudentGuidence />
+        <WonderfulMoment />
+    </div>
 </template>
+<script setup>
+import CurriculumsIntroduction from '../../components/DesktopEducationTeaching/CurriculumsIntroduction.vue'
+import LearningMethod from '../../components/DesktopEducationTeaching/LearningMethod.vue'
+import StudentGuidence from '../../components/DesktopEducationTeaching/StudentGuidence.vue'
+import WonderfulMoment from '../../components/DesktopEducationTeaching/WonderfulMoment.vue'
+</script>
