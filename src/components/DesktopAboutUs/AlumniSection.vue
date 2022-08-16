321bee7cc9ca0b3556543c1754d26c1099397c43
lihe07 <li@imlihe.com>
2022-08-16 20:36:29 +0800

    Fixes on view: About

--- src/components/DesktopAboutUs/AlumniSection.vue
+++ src/components/DesktopAboutUs/AlumniSection.vue
@@ -1,53 +1,171 @@
 <template>
-    <section class="section">
-        <div id="alumni" class="p-y-10 font-sans flex-col items-center">
-            <!-- <h2 class="title">
+  <section class="section">
+    <div id="alumni" class="p-y-10 font-sans flex-col items-center">
+      <!-- <h2 class="title">
                 {{ $t("AboutUs.Alumni.Title") }}
             </h2> -->
-            <FancyTitle :top="$t('AboutUs.Alumni.Title')" btm="Alumni" color="red"></FancyTitle>
-            <div class="flex">
-                <div style="flex: 1" class="pr-5 flex pl-8 items-center">
-                    <img :src="homeBg1" style="max-width: 100%;
-        max-height: 100%;" alt="pic1" class="alumni-big-pic" />
-                </div>
-                <div style="flex: 2" class="flex-col pl-5">
-                    <div>
-                        <div class="text-9 font-700">“</div>
-                        <p class="p-5 content">{{ $t("AboutUs.Alumni.Content") }}</p>
-
-                        <div class="text-9 font-700 text-right">”</div>
-                    </div>
-                    <div class="flex">
-                        <div class="alumni-small-pic">
-                            <img :src="homeBg1" style="max-width: 100%;
-        max-height: 100%;" alt="pic1" />
-                        </div>
-                        <div class="alumni-small-pic">
-                            <img :src="homeBg1" style="max-width: 100%;
-        max-height: 100%;" alt="pic1" />
-                        </div>
-                        <div class="alumni-small-pic">
-                            <img :src="homeBg1" style="max-width: 100%;
-        max-height: 100%;" alt="pic1" />
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <FancyTitle
+        :top="$t('AboutUs.Alumni.Title')"
+        btm="Alumni"
+        color="red"
+      ></FancyTitle>
+      <div class="flex">
+        <div style="flex: 1" class="pr-5 flex pl-8 items-center">
+          <img
+            :src="homeBg1"
+            style="max-width: 100%; max-height: 100%"
+            alt="pic1"
+            class="alumni-big-pic"
+          />
         </div>
-    </section>
+        <div style="flex: 2" class="flex-col pl-5">
+          <div class="relative">
+            <!-- Left top quote -->
+            <div class="absolute left-20px top-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                width="56.55765920826161"
+                height="53"
+                viewBox="0 0 56.55765920826161 53"
+                fill="none"
+              >
+                <defs>
+                  <rect
+                    id="path_0"
+                    x="0"
+                    y="0"
+                    width="56.55765920826162"
+                    height="53"
+                  />
+                </defs>
+                <g
+                  opacity="1"
+                  transform="translate(0 0)  rotate(0 28.27882960413081 26.5)"
+                >
+                  <mask id="bg-mask-0" fill="white">
+                    <use xlink:href="#path_0"></use>
+                  </mask>
+                  <g mask="url(#bg-mask-0)">
+                    <g
+                      opacity="1"
+                      transform="translate(0 0)  rotate(0 28.27882960413081 26.5)"
+                    >
+                      <path
+                        id="路径 1"
+                        fill-rule="evenodd"
+                        style="fill: #122a28"
+                        transform="translate(31.867098865540292 0)  rotate(0 12.345280171360663 26.5)"
+                        opacity="1"
+                        d="M0,53L8.98,53C17.66,53 24.69,46.41 24.69,38.28L24.69,27.75L12.8,27.75C13.1,19.28 19.42,11.52 24.69,9.99L24.69,0C15.06,1.8 0,11.79 0,28.72L0,53Z "
+                      />
+                      <path
+                        id="路径 2"
+                        fill-rule="evenodd"
+                        style="fill: #122a28"
+                        transform="translate(0 0)  rotate(0 12.345280171360665 26.5)"
+                        opacity="1"
+                        d="M0,53L8.83,53C17.51,53 24.54,46.41 24.54,38.28L24.54,27.75L12.8,27.75C13.1,19.28 19.42,11.52 24.69,9.99L24.69,0C15.06,1.8 0,11.79 0,28.72L0,53Z "
+                      />
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </div>
+            <p class="p-20 content">{{ $t('AboutUs.Alumni.Content') }}</p>
+            <!-- Right bottom quote -->
+            <div class="absolute right-20px bottom-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                width="56.55765920826161"
+                height="53"
+                viewBox="0 0 56.55765920826161 53"
+                fill="none"
+              >
+                <defs>
+                  <rect
+                    id="path_0"
+                    x="0"
+                    y="0"
+                    width="56.55765920826162"
+                    height="53"
+                  />
+                </defs>
+                <g
+                  opacity="1"
+                  transform="translate(0 0)  rotate(0 28.27882960413081 26.5)"
+                >
+                  <mask id="bg-mask-0" fill="white">
+                    <use xlink:href="#path_0"></use>
+                  </mask>
+                  <g mask="url(#bg-mask-0)">
+                    <g
+                      opacity="1"
+                      transform="translate(0 0)  rotate(0 28.27882960413081 26.5)"
+                    >
+                      <path
+                        id="路径 1"
+                        fill-rule="evenodd"
+                        style="fill: #122a28"
+                        transform="translate(0 0)  rotate(0 12.345280171360665 26.5)"
+                        opacity="1"
+                        d="M24.69,0L15.71,0C7.03,0 0,6.59 0,14.72L0,25.25L11.89,25.25C11.59,33.72 5.27,41.48 0,43.01L0,53C9.64,51.2 24.69,41.21 24.69,24.28L24.69,0Z "
+                      />
+                      <path
+                        id="路径 2"
+                        fill-rule="evenodd"
+                        style="fill: #122a28"
+                        transform="translate(31.867098865540356 0)  rotate(0 12.345280171360667 26.5)"
+                        opacity="1"
+                        d="M24.69,0L15.86,0C7.18,0 0.15,6.59 0.15,14.72L0.15,25.25L11.89,25.25C11.59,33.72 5.27,41.48 0,43.01L0,53C9.64,51.2 24.69,41.21 24.69,24.28L24.69,0Z "
+                      />
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </div>
+          </div>
+          <div class="flex">
+            <div class="m-4" v-for="alumn in alumni" :key="alumn">
+              <img
+                :src="alumn.image"
+                style="max-width: 100%; max-height: 100%"
+                alt="pic1"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
 import homeBg1 from '../../assets/images/homeBg1.jpg?webp'
 import FancyTitle from '../FancyTitle.vue'
+import QuoteMessage from '../QuoteMessage.vue'
+
+const alumni = [
+  {
+    image: homeBg1,
+    'zh-CN':
+      '毕业生1介绍 Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dolor ipsam tenetur officia, quod incidunt ad beatae nemo consequatur ullam esse doloribus minus molestias a laudantium excepturi accusamus voluptatem reiciendis.',
+    'en-US':
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dolor ipsam tenetur officia, quod incidunt ad beatae nemo consequatur ullam esse doloribus minus molestias a laudantium excepturi accusamus voluptatem reiciendis.'
+  },
+  {
+    image: homeBg1,
+    'zh-CN':
+      '毕业生2介绍 Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dolor ipsam tenetur officia, quod incidunt ad beatae nemo consequatur ullam esse doloribus minus molestias a laudantium excepturi accusamus voluptatem reiciendis.',
+    'en-US':
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dolor ipsam tenetur officia, quod incidunt ad beatae nemo consequatur ullam esse doloribus minus molestias a laudantium excepturi accusamus voluptatem reiciendis.'
+  }
+]
 </script>
 
 <style>
 .alumni-big-pic {
-    box-shadow: -2rem 2rem var(--standard-red), -0.25rem 0.25rem 0.25rem grey;
-}
-
-.alumni-small-pic {
-    margin: 1rem;
+  box-shadow: -2rem 2rem var(--standard-red), -0.25rem 0.25rem 0.25rem grey;
 }
 </style>
--- src/components/FancyTitle.vue
+++ src/components/FancyTitle.vue
@@ -1,24 +1,40 @@
 <template>
-  <div class="w-max">
+  <div class="w-max" :class="{ 'text-right': right }">
     <h1 class="color-#122A28 m-0 text-10 font-title">
-      <span v-for="word in top" :key="word" class="m-r-5">
+      <span
+        v-for="word in top"
+        :key="word"
+        class="m-r-5"
+        :class="{ 'important:m-r-0': right }"
+      >
         <span>{{ word.left }}</span>
-        <span :class="{
-          'color-[var(--standard-blue)]': props.color === 'blue',
-          'color-[var(--standard-red)]': props.color === 'red'
-        }">{{ word.colored }}</span>
+        <span
+          :class="{
+            'color-[var(--standard-blue)]': props.color === 'blue',
+            'color-[var(--standard-red)]': props.color === 'red'
+          }"
+          >{{ word.colored }}</span
+        >
         <span>{{ word.right }}</span>
       </span>
     </h1>
-    <div class="m-l-8">
+    <div class="m-l-8" :class="{ 'important:m-l-0': right }">
       <hr class="border-width-2 border-solid border-color-#122A28" />
       <h1 class="color-#122A28 m-0 text-9 font-title font-300">
-        <span v-for="word in btm" :key="word" class="m-r-5">
+        <span
+          v-for="word in btm"
+          :key="word"
+          class="m-r-5"
+          :class="{ 'important:m-r-15': right }"
+        >
           <span>{{ word.left }}</span>
-          <span :class="{
-            'color-[var(--standard-blue)]': props.color === 'blue',
-            'color-[var(--standard-red)]': props.color === 'red'
-          }">{{ word.colored }}</span>
+          <span
+            :class="{
+              'color-[var(--standard-blue)]': props.color === 'blue',
+              'color-[var(--standard-red)]': props.color === 'red'
+            }"
+            >{{ word.colored }}</span
+          >
           <span>{{ word.right }}</span>
         </span>
       </h1>
@@ -29,7 +45,7 @@
 <script setup>
 import { computed } from 'vue'
 
-const props = defineProps(['top', 'btm', 'color'])
+const props = defineProps(['top', 'btm', 'color', 'right'])
 
 function compute (string) {
   return string.split(' ').map((word) => {
@@ -39,4 +55,10 @@ function compute (string) {
 
 const top = computed(() => compute(props.top))
 const btm = computed(() => compute(props.btm))
+const right = computed(() => {
+  if (props.right === undefined) {
+    return false
+  }
+  return true
+})
 </script>
