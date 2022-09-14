0c7b0135daa4dbc48b20629c0f0f160335b14141
lihe07 <li@imlihe.com>
2022-09-14 17:19:22 +0800

    update

--- src/views/desktop/AboutUs.vue
+++ src/views/desktop/AboutUs.vue
@@ -13,8 +13,10 @@
     <div class="bg-[var(--standard-blue)]">
       <TestCenterSection />
     </div>
-    <div :style="{ backgroundImage: 'url(' + homeBg2 + ')' }"
-      style="background-size: cover; background-position: center">
+    <div
+      :style="{ backgroundImage: 'url(' + homeBg2 + ')' }"
+      style="background-size: cover; background-position: center"
+    >
       <FacultySection />
     </div>
     <QuoteSection />
