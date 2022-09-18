5b84c348c13d18bd669be2d8d5afbef90cc391d5
lihe07 <li@imlihe.com>
2022-09-18 17:59:23 +0800

    Using native CSS transitions

--- src/components/DesktopCampusLife/EventsImg.vue
+++ src/components/DesktopCampusLife/EventsImg.vue
@@ -1,60 +1,22 @@
 <template>
-  <div class="w-full h-full" @mouseenter="enter()" @mouseleave="leave()">
+  <div class="w-full h-full">
     <img
       :src="props.image"
       class="block w-full h-full object-cover"
       alt="Bg1"
     />
-    <Transition>
-      <div
-        class="absolute bg-black opacity-50"
-        style="bottom: 0px; left: 0px; right: 0px; top: 0px"
-        v-if="hover"
-      ></div>
-    </Transition>
-    <Transition>
-      <div
-        class="absolute color-white"
-        style="bottom: 32px; left: 32px; right: 32px"
-        v-if="hover"
-      >
+    <div
+      class="absolute bottom-0 bg-dark/30 h-full w-full hover:op-100 op-0 transition-opacity-500 flex items-end"
+    >
+      <div class="color-white m-10">
         <h3>{{ props.name }}</h3>
         <p>
           {{ props.description }}
         </p>
       </div>
-    </Transition>
+    </div>
   </div>
 </template>
 <script setup>
-import { ref } from 'vue'
-
-const hover = ref(false)
 const props = defineProps(['image', 'name', 'description'])
-
-function enter () {
-  hover.value = true
-  console.log(hover)
-}
-
-function leave () {
-  hover.value = false
-  console.log(hover)
-}
 </script>
-<style>
-.v-enter-active {
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  transition-duration: 750ms;
-}
-
-.v-leave-active {
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  transition-duration: 250ms;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-</style>
