2ba2cd6ec6b1d5d4bfd82adc9650b6a1b2d9df52
lihe07 <li@imlihe.com>
2022-10-12 07:24:52 +0800

    HomePage

--- src/views/mobile/HomePage.vue
+++ src/views/mobile/HomePage.vue
@@ -2,9 +2,15 @@
   <div>
     <!-- BG -->
     <HomeBackground />
+
+    <!--  -->
+    <section class="p-3">
+      <MobileCard> </MobileCard>
+    </section>
   </div>
 </template>
 
 <script setup>
 import HomeBackground from '../../components/MobileHomePage/HomeBackground.vue'
+import MobileCard from '../../components/MobileCard.vue'
 </script>
