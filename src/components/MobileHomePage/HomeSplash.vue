700b2dc034f8c03f1c3dbbf047a5e7fe4421f35f
lihe07 <li@imlihe.com>
2022-10-25 15:30:56 +0800

    HomeSplash

---
+++ src/components/MobileHomePage/HomeSplash.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="relative">
+    <slot></slot>
+    <div class="fixed">
+      <!-- Foreground -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+import img1 from '../../assets/images/HomePage/Autumn.jpg'
+import img2 from '../../assets/images/HomePage/Dongba1.jpg'
+
+const images = [img1, img2]
+</script>
