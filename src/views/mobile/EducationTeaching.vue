a623d0a896d545d03bb799f4b8fa67b7de13a7a0
Baa~ <91712540+ShepherdShepherd@users.noreply.github.com>
2022-11-30 10:28:21 +0800

    Add files via upload

--- src/views/mobile/EducationTeaching.vue
+++ src/views/mobile/EducationTeaching.vue
@@ -1,15 +1,24 @@
 <template>
+
   <div>
-    <HomeBackground />
-    <MobileHead :img="imgHeader1">
-      {{ $t('views.EducationTeaching') }}</MobileHead
-    >
-    <section class="p-3">TO-DO: Content Here</section>
+    <open class="absolute"></open>
+    <section class="p-3 snap-y snap-proximity overflow-y-scroll">
+      <Curriculums class="snap-start"></Curriculums>
+      <LearningMethod class="snap-start"></LearningMethod>
+      <StudentGuidence class="snap-start"></StudentGuidence>
+      <WonderfulMoment class="snap-start"></WonderfulMoment>
+    </section>
   </div>
 </template>
 
 <script setup>
+import HomeBackground from '../../components/MobileHomePage/HomeBackground.vue'
+import open from '../../components/MobileEducationTeaching/EducationTeachingOpen.vue'
+import Curriculums from '../../components/MobileEducationTeaching/Curriculums.vue'
+import LearningMethod from '../../components/MobileEducationTeaching/LearningMethod.vue'
+import StudentGuidence from '../../components/MobileEducationTeaching/StudentGuidence.vue'
+import WonderfulMoment from '../../components/MobileEducationTeaching/Wonderful moment.vue'
+import MobileCard from '../../components/MobileCard.vue'
 import MobileHead from '../../components/MobileHead.vue'
-
-import imgHeader1 from '../../assets/images/EducationTeaching/Header_1.jpg?webp'
+import firstImg from '../../assets/images/HomePage/Autumn.jpg'
 </script>
