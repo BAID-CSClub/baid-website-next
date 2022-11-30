701ce8c6ca16a3c2389cd46d4f9271423ee0d972
Baa~ <91712540+ShepherdShepherd@users.noreply.github.com>
2022-11-30 12:41:55 +0800

    Add files via upload

--- src/views/mobile/EducationTeaching.vue
+++ src/views/mobile/EducationTeaching.vue
@@ -13,7 +13,7 @@
 
 <script setup>
 import open from '../../components/MobileEducationTeaching/EducationTeachingOpen.vue'
-import Curriculums from '../../components/MobileEducationTeaching/CurriculumsInroduction.vue'
+import Curriculums from '../../components/MobileEducationTeaching/CurriculumsIntroduction.vue'
 import LearningMethod from '../../components/MobileEducationTeaching/LearningMethod.vue'
 import StudentGuidence from '../../components/MobileEducationTeaching/StudentGuidence.vue'
 import WonderfulMoment from '../../components/MobileEducationTeaching/WonderfulMoment.vue'
