502f0bb265c278d0c8c66416a5768cbd7e2ce839
lihe07 <li@imlihe.com>
2022-12-01 16:04:25 +0800

    Fix

---
+++ src/components/MobileStudentLife/SplashBlock.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="flex justify-center items-center flex-col h-50% w-full bg-cover"
+    :style="{
+      'background-image': `url(${props.image})`
+    }"
+  >
+    <h1 class="color-white font-title text-10">
+      <span>{{ props.title[0] }}</span>
+      <span class="color-yellow">{{ props.title[1] }}</span>
+      <span>{{ props.title.substr(2) }}</span>
+    </h1>
+    <a :href="props.href" class="decoration-none color-yellow">了解更多</a>
+  </div>
+</template>
+<script setup>
+const props = defineProps(['title', 'href', 'image'])
+</script>
--- src/views/mobile/StudentLife.vue
+++ src/views/mobile/StudentLife.vue
@@ -1,28 +1,12 @@
 <template>
   <div class="w-full h-[calc(100vh-5rem)] mt-20">
-    <div
-      class="flex justify-center items-center flex-col h-50% w-full bg-cover"
-      :style="{
-        'background-image': `url(${camping})`
-      }"
-    >
-      <h1 class="setColorH1">学生社团</h1>
-      <a href="">了解更多</a>
-    </div>
-
-    <div
-      class="flex justify-center items-center flex-col h-50% w-full bg-cover"
-      :style="{
-        'background-image': `url(${arrow})`
-      }"
-    >
-      <h1 class="setColorH1">学生活动</h1>
-      <a href="">了解更多</a>
-    </div>
+    <SplashBlock title="学生社团" href="#" :image="camping"></SplashBlock>
+    <SplashBlock title="特色活动" href="#" :image="archery"></SplashBlock>
   </div>
 </template>
 
 <script setup>
 import camping from '../../assets/images/StudentLife/Activities/Camping.jpg?webp'
-import arrow from '../../assets/images/StudentLife/Activities/ZWTZY.jpg?webp'
+import archery from '../../assets/images/StudentLife/Activities/ZWTZY.jpg?webp'
+import SplashBlock from '../../components/MobileStudentLife/SplashBlock.vue'
 </script>
