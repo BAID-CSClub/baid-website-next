c40cea11281bd4de80c048b7481aadb8048dc801
Henry3510 <30743133+Henry3510@users.noreply.github.com>
2022-12-18 10:42:20 +0800

    fix

--- src/components/DesktopEducationTeaching/CurriculumBlock.vue
+++ src/components/DesktopEducationTeaching/CurriculumBlock.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="m-y-4">
     <div class="flex items-center">
-      <img :src="number" class="w-18 block" />
-      <h3 class="m-x-4 text-6">{{ $t(title) }}</h3>
+      <img :src="props.number" class="w-18 block" />
+      <h3 class="m-x-4 text-6">{{ $t(props.title) }}</h3>
     </div>
     <div class="flex flex-col items-center m-y-4">
       <pre class="w-90%">
-        {{ $t(description) }}
+        {{ $t(props.description) }}
       </pre>
     </div>
   </div>
