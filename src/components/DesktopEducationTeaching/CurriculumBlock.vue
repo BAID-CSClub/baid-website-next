d789abd4f7215bf6c0b2b3cb62b880d88c8c37ce
Henry3510 <30743133+Henry3510@users.noreply.github.com>
2022-12-18 10:40:35 +0800

    Update content & fix

---
+++ src/components/DesktopEducationTeaching/CurriculumBlock.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="m-y-4">
+    <div class="flex items-center">
+      <img :src="number" class="w-18 block" />
+      <h3 class="m-x-4 text-6">{{ $t(title) }}</h3>
+    </div>
+    <div class="flex flex-col items-center m-y-4">
+      <pre class="w-90%">
+        {{ $t(description) }}
+      </pre>
+    </div>
+  </div>
+</template>
+<script setup>
+const props = defineProps(['number', 'title', 'description'])
+</script>
