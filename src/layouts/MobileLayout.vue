f5169c52b7d172ccf176e3cce7570a79d8814b35
Henry3510 <30743133+Henry3510@users.noreply.github.com>
2023-01-15 18:52:50 +0800

    Update MobileFooter

--- src/layouts/MobileLayout.vue
+++ src/layouts/MobileLayout.vue
@@ -5,9 +5,11 @@
       <slot></slot>
     </main>
   </MobileSplash>
+  <!-- <MobileFooter /> -->
 </template>
 
 <script setup>
+// import MobileFooter from '../components/MobileFooter.vue'
 import MobileHeader from '../components/MobileHeader.vue'
 import MobileSplash from '../components/MobileSplash.vue'
 </script>
