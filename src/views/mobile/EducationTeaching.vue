cf0db920e36f4488c62790e8b784835a66e475f5
lihe07 <li@imlihe.com>
2023-01-15 20:55:18 +0800

    Fixes

--- src/views/mobile/EducationTeaching.vue
+++ src/views/mobile/EducationTeaching.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="font-sans" :class="{'overflow-y-clip h-100vh':clip}">
-    <Splash @clip="clip=false"/>
+  <div class="font-sans">
+    <Splash />
     <section class="p-3 snap-y snap-proximity overflow-y-scroll">
       <Curriculums class="snap-start"></Curriculums>
       <LearningMethod class="snap-start"></LearningMethod>
@@ -16,7 +16,4 @@ import Curriculums from '../../components/MobileEducationTeaching/CurriculumsInt
 import LearningMethod from '../../components/MobileEducationTeaching/LearningMethod.vue'
 import StudentGuidance from '../../components/MobileEducationTeaching/StudentGuidance.vue'
 import WonderfulMoment from '../../components/MobileEducationTeaching/WonderfulMoments.vue'
-
-import { ref } from 'vue'
-const clip = ref(true)
 </script>
