09479efb6620c28c77034c5d0f5751ca02e4ca84
lihe07 <li@imlihe.com>
2023-08-18 20:15:30 +0800

    join us

--- src/components/DesktopJoinUs/JoinStudentSection.vue
+++ src/components/DesktopJoinUs/JoinStudentSection.vue
@@ -3,24 +3,16 @@
     <div>
       <h3 class="text-8">{{ $t('JoinUs.Student.Title') }}</h3>
       <pre v-html="pageData.student"></pre>
-      <div
-        class="flex items-center justify-center text-8 m-t-10 py-10 bg-[var(--standard-red)] rd-5 color-white"
-      >
+      <div class="flex items-center justify-center text-8 m-t-10 py-10 bg-[var(--standard-red)] rd-5 color-white">
         <div style="flex: 3" class="text-center">{{ $t('JoinUs.Join') }}</div>
         <div style="flex: 1" class="text-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="48"
-            width="48"
-            fill="white"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48" fill="white">
             <path
-              d="M22.95 38.9q-.4-.4-.4-1.05t.4-1.05l11.3-11.3H9.5q-.65 0-1.075-.425Q8 24.65 8 24q0-.65.425-1.075Q8.85 22.5 9.5 22.5h24.75l-11.3-11.3q-.4-.4-.4-1.075 0-.675.4-1.075.4-.4 1.05-.4t1.05.4l13.9 13.9q.25.25.35.5.1.25.1.55 0 .25-.1.525t-.35.525l-13.9 13.9q-.4.4-1.05.375-.65-.025-1.05-.425Z"
-            />
+              d="M22.95 38.9q-.4-.4-.4-1.05t.4-1.05l11.3-11.3H9.5q-.65 0-1.075-.425Q8 24.65 8 24q0-.65.425-1.075Q8.85 22.5 9.5 22.5h24.75l-11.3-11.3q-.4-.4-.4-1.075 0-.675.4-1.075.4-.4 1.05-.4t1.05.4l13.9 13.9q.25.25.35.5.1.25.1.55 0 .25-.1.525t-.35.525l-13.9 13.9q-.4.4-1.05.375-.65-.025-1.05-.425Z" />
           </svg>
         </div>
         <div style="flex: 3" class="text-center color-[var(--standard-red)]">
-          <div class="bg-white rd-5 p-3" style="display: inline-block">
+          <div class="bg-white rd-5 p-3" style="display: inline-block" @click="open(pageData.student_portal)">
             {{ $t('JoinUs.Student.Join') }}
           </div>
         </div>
@@ -31,4 +23,10 @@
 <script setup>
 import { inject } from 'vue'
 const pageData = inject('data')
+
+
+function open(url) {
+  window.open(url)
+}
 </script>
+
