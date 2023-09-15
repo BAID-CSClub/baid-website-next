c6b11d199913413a40b3bc5f152b85302659f971
lihe07 <li@imlihe.com>
2023-09-15 17:45:56 +0800

    EducationPhilosophy

--- src/components/DesktopHomePage/EducationPhilosophy.vue
+++ src/components/DesktopHomePage/EducationPhilosophy.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="bg-[var(--standard-blue)] mb-15">
-    <div class="section ">
-      <NotFancyTitle cn="办学理念" en="Education Philosophy" color="white"></NotFancyTitle>
+    <div class="section">
+      <NotFancyTitle
+        cn="办学理念"
+        en="Education Philosophy"
+        color="white"
+      ></NotFancyTitle>
       <div class="flex m-t-10 m-auto justify-items-center">
-        <PhilosophyBlock v-for="(key, index) in blocks" :key="key" :title="pageData[key + '_title']"
-          :content="pageData[key + '_content']" />
+        <PhilosophyBlock
+          v-for="(key, index) in blocks"
+          :key="key"
+          :title="pageData[key + '_title']"
+          :content="pageData[key + '_content']"
+          image="https://images.unsplash.com/photo-1693047995769-80b8b4af4eee"
+          :index="index"
+          :active="active"
+          @activate="active = index"
+          @deactivate="active = null"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { inject } from 'vue'
+import { inject, ref } from 'vue'
 import NotFancyTitle from '../NotFancyTitle.vue'
 import PhilosophyBlock from './PhilosophyBlock.vue'
 
 const pageData = inject('data')
 const blocks = ['motto', 'spirit', 'key_competency', 'cultivation']
+
+const active = ref(null)
 
 // pageData[key + "_title, _content"]
 </script>
--- src/components/DesktopHomePage/PhilosophyBlock.vue
+++ src/components/DesktopHomePage/PhilosophyBlock.vue
@@ -1,14 +1,110 @@
 <template>
-  <div class="flex-1 h-100 color-white">
-    <div class="m-5">
-      <h2 class="text-8 !m-0 tracking-wide">
+  <div
+    class="h-100 color-white bg-cover relative transition-all transition-duration-.3s"
+    :style="{ backgroundImage: 'url(' + image + ')' }"
+    :class="{ 'w-33%': thisActive, 'w-25%': !thisActive }"
+    @mousemove="$emit('activate')"
+    @mouseleave="$emit('deactivate')"
+  >
+    <div
+      class="w-full h-full bg-#103c74 bg-op-70 transition transition-duration-.5s relative"
+      :class="{ '!bg-op-10': thisActive }"
+    >
+      <pre
+        class="text-6 leading-normal text-right"
+        :class="{ 'op-0': !delay, 'op-100': delay }"
+        >{{ props.content }}</pre
+      >
+      <h2
+        class="text-8 !m-0 tracking-wide text-center write-vertical-left"
+        :class="{ center: !thisActive, right: thisActive }"
+      >
         {{ props.title }}
       </h2>
-      <!-- <pre class="text-6 leading-normal text-right">{{ props.content }}</pre> -->
     </div>
   </div>
 </template>
 
 <script setup>
-const props = defineProps(['title', 'content', 'blue'])
+import { computed, watch, ref } from 'vue'
+
+const props = defineProps([
+  'title',
+  'content',
+  'image',
+  'index',
+  'active',
+  'deactivate'
+])
+
+const thisActive = computed(() => props.active === props.index)
+
+const delay = ref(false)
+
+let timeout
+watch(thisActive, (a) => {
+  if (a) {
+    timeout = setTimeout(() => (delay.value = true), 300)
+  } else {
+    clearTimeout(timeout)
+    delay.value = false
+  }
+})
 </script>
+
+<style scoped>
+pre {
+  position: absolute;
+
+  left: 30px;
+
+  top: 50%;
+
+  text-align: left;
+
+  transform: translateY(-50%);
+
+  width: 200px;
+
+  transition: all 0.5s;
+}
+
+h2 {
+  position: absolute;
+  top: 50%;
+  transform: translate(50%, -50%);
+
+  transition: all 0.5s;
+}
+
+h2::before {
+  content: '';
+  position: absolute;
+  width: 4px;
+  height: 70%;
+
+  top: 50%;
+  transform: translateY(-50%);
+  transition: all 0.5s;
+
+  background-color: var(--standard-red);
+}
+
+.center {
+  right: 50%;
+}
+
+.center::before {
+  opacity: 0;
+  height: 0;
+}
+
+.right {
+  right: 60px;
+}
+
+.right::before {
+  opacity: 1;
+  height: 70%;
+}
+</style>
