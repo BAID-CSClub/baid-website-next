8bd9e9eb062ace479cde2733a46e0dcaf13dfd6a
lihe07 <li@imlihe.com>
2023-09-16 18:09:40 +0800

    Curriculums

---
+++ src/components/DesktopEducationTeaching/CurriculumContent.vue
@@ -0,0 +1,6 @@
+<template>
+  <img
+    src="https://images.unsplash.com/photo-1610296669228-602fa827fc1f"
+    alt=""
+  />
+</template>
--- src/components/DesktopEducationTeaching/CurriculumsIntroduction.vue
+++ src/components/DesktopEducationTeaching/CurriculumsIntroduction.vue
@@ -1,35 +1,89 @@
 <template>
   <div class="section">
     <NotFancyTitle cn="课程体系" en="Curriculums" color="blue"></NotFancyTitle>
-    <div class="flex gap-16">
-      <div class="w-full">
-        <CurriculumBlock
-          :number="number1"
-          title="EducationTeaching.Curriculums.1.Title"
-          description="EducationTeaching.Curriculums.1.Description"
-        ></CurriculumBlock>
-        <CurriculumBlock
-          :number="number2"
-          title="EducationTeaching.Curriculums.2.Title"
-          description="EducationTeaching.Curriculums.2.Description"
-        ></CurriculumBlock>
+    <div class="flex gap-16 h-80">
+      <div class="w-40% flex flex-col justify-around">
+        <div
+          v-for="(c, i) in pageData.curriculums"
+          class="op-70 container w-50"
+          :class="{ '!op-100 active': current == i }"
+          @mouseover="current = i"
+        >
+          <h2 class="m0 w-max">{{ c.name }}</h2>
+
+          <div
+            class="absolute top-10 left-0 op-0"
+            :class="{ '!op-100': current == i }"
+          >
+            <ReadMore />
+          </div>
+        </div>
       </div>
-      <hr />
-      <div class="w-full">
-        <CurriculumBlock
-          :number="number3"
-          title="EducationTeaching.Curriculums.3.Title"
-          description="EducationTeaching.Curriculums.3.Description"
-        ></CurriculumBlock>
+
+      <div class="relative w-full">
+        <div
+          class="w-full flex flex-col justify-center gap-5 absolute top-0 op-0 h-full"
+          v-for="(c, i) in pageData.curriculums"
+          :class="{ '!op-100': current == i, 'flex-col-reverse': i % 2 == 0 }"
+        >
+          <div
+            class="flex w-full gap-5"
+            :class="{ 'flex-row-reverse': i % 2 == 0 }"
+          >
+            <img
+              src="https://images.unsplash.com/photo-1610296669228-602fa827fc1f?w=500"
+              class="flex-1 w-full h-50"
+            />
+            <img
+              src="https://images.unsplash.com/photo-1688362378188-264c2d01ae9d?w=500"
+              class="flex-1 w-full h-50"
+            />
+          </div>
+          <pre class="" v-html="c.content"></pre>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script setup>
+import { ref, inject } from 'vue'
 import NotFancyTitle from '../NotFancyTitle.vue'
-import CurriculumBlock from './CurriculumBlock.vue'
+import ReadMore from '@/components/ReadMore.vue'
 
-import number1 from '../../assets/images/Number_1.svg'
-import number2 from '../../assets/images/Number_2.svg'
-import number3 from '../../assets/images/Number_3.svg'
+const pageData = inject('data')
+
+const current = ref(0)
 </script>
+
+<style scoped>
+.container {
+  position: relative;
+  height: 50px;
+  left: 0;
+}
+
+h2::before {
+  content: '';
+  display: block;
+  position: absolute;
+
+  background-color: var(--standard-blue);
+  width: 5px;
+  height: 85px;
+  top: -5px;
+  left: -20px;
+  opacity: 0;
+}
+
+.active h2::before {
+  opacity: 1;
+}
+
+.active {
+  left: 20px;
+}
+
+* {
+  transition: all 0.3s ease-in-out;
+}
+</style>
