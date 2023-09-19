abc31acda9f76879ca324e171ac2040f90372bdf
Henry3510 <Henry3510@users.noreply.github.com>
2023-09-19 08:23:45 +0000

    deploy: 2d4a55fe4657590258e322b85d953de224e52e9a

---
+++ assets/create-element-if-not-defined-8c777f51.js
@@ -0,0 +1,1 @@
+import{g as m}from"./swiper.min-ae69d5b6.js";function i(l,f,n,c){const o=m();return l.params.createElements&&Object.keys(c).forEach(t=>{if(!n[t]&&n.auto===!0){let e=l.$el.children(`.${c[t]}`)[0];e||(e=o.createElement("div"),e.className=c[t],l.$el.append(e)),n[t]=e,f[t]=e}}),n}export{i as c};
