a094848fa493e382ad2df980c3f1304146243260
lihe07 <li@imlihe.com>
2023-10-25 10:16:36 +0800

    JoinFacultySection

---
+++ src/components/DesktopJoinUs/ImageCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-80 bg-cover bg-center flex items-center" :style="{ 'background-image': `url(${props.bg})` }">
+    <div class="bg-white/80 px-5 py-8 mr-20 backdrop-blur-5">
+      <h2 class="mb-3 mt-0">{{ props.title }}</h2>
+      <p>
+        <slot />
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["bg", "title"])
+</script>
