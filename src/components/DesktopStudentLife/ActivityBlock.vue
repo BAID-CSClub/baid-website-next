3ad6308d07cd0513b5110336b94a53b14cd59bdd
Dreta <32196824+Dreta@users.noreply.github.com>
2023-11-26 10:46:31 +0800

    update design of activities section

---
+++ src/components/DesktopStudentLife/ActivityBlock.vue
@@ -0,0 +1,12 @@
+<template>
+  <ActivityTextBlock v-if="props.textAlign === 'left'" :name="props.activity.name" :description="props.activity.content"
+                     :background="props.background" :light="props.light" />
+  <img :src="props.activity.image" class="w-full h-80 object-cover object-center">
+  <ActivityTextBlock v-if="props.textAlign === 'right'" :name="props.activity.name" :description="props.activity.content"
+                     :background="props.background" :light="props.light" />
+</template>
+<script setup>
+import ActivityTextBlock from '@/components/DesktopStudentLife/ActivityTextBlock.vue'
+
+const props = defineProps(['activity', 'background', 'light', 'text-align'])
+</script>
---
+++ src/components/DesktopStudentLife/ActivityTextBlock.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full h-80 p-8" :style="{'background': props.background, 'color': props.light ? 'white': 'black'}">
+    <h3 class="text-2xl font-bold">{{ props.name }}</h3>
+    <hr :class="['mb-4', props.light ? 'border-white/30' : 'border-black/30']" />
+    <div class="no-size text-sm" v-html="props.description"></div>
+  </div>
+</template>
+<script setup>
+const props = defineProps(['name', 'description', 'background', 'light'])
+</script>
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+</style>
